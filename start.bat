4c7707d3ecda3e3b83f6344964f0bb5b233ba034
neohral <honekingu29@gmail.com>
2021-12-30 01:57:39 +0900

    Update start.bat
    server.cfgの場所を追記

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-srcds.exe -console -autoupdate -game left4dead2 +hostport 27016 +map "c1m1_hotel coop" +maxplayers 10 +exec server.cfg
+srcds.exe -console -autoupdate -game left4dead2 +hostport 27016 +map "c1m1_hotel coop" +maxplayers 10 +exec left4dead2/cfg/server.cfg
